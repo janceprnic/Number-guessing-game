7d0a224d8e6fb811faee070c790e15b1aed80c0a
Jan Čeprnić <68035779+janceprnic@users.noreply.github.com>
2022-08-21 19:41:41 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
+//Generating a random number
 let random_num = Math.floor(Math.random()*15 + 1);
 
-let btn = document.getElementById('guess-btn');
+//Creating variables for the 2 buttons, an input and guess counter
+let guess_btn = document.querySelector('.guess-btn');
 
-let input = document.getElementById('num-input');
+let input = document.querySelector('.num-input');
 
-let again = document.getElementById('again');
-again.disabled = true;
-again.style.cursor = "not-allowed";
+let play_again = document.querySelector('.play-again');
 
 let guesses = 0;
 
-btn.addEventListener('click' ,function () {
-    let input = document.getElementById('num-input').value;
+//The game functionality which activates after you click the "guess button"
+guess_btn.addEventListener('click' ,function () {
+    
+    let input = document.querySelector('.num-input').value;
 
     if(input == 0 || input < 1 || input > 15 ) {
         alert("You must enter a number between 1 and 15!");
@@ -27,14 +29,16 @@ btn.addEventListener('click' ,function () {
 
     else if (input == random_num) {
         alert(`You won! You guessed the number in ${guesses} tries.`);
-        btn.remove();
-        again.disabled = false;
-        again.style.cursor = "pointer";
+
+        guess_btn.remove();
+
+        play_again.style.display = "block";
     }
 
     guesses++;
 })
 
-again.addEventListener('click', function() {
+//Function that refreshes the page and sets a new random number when you click the "Play Again?" button
+play_again.addEventListener('click', function() {
     window.location.reload();
 })
